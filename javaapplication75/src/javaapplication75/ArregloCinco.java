8482a0f848d26bfa5db41ec4b957db6bce959a6f
reroes <reroes799@gmail.com>
2019-05-30 08:41:28 -0500

    manejo de estructuras unidimensionales en Java

---
+++ javaapplication75/src/javaapplication75/ArregloCinco.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication75;
+
+/**
+ *
+ * @author reroes
+ */
+public class ArregloCinco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int[] c = {10, 20, 30, 40}; // declaramos la variable
+      
+        System.out.printf("%s\t\t%s\n", "Indice", "Valor");
+
+        for (int contador = 0; contador < c.length; contador++) {
+            System.out.printf("%d\t\t%d\n", contador, c[contador]);
+        }
+    }
+
+}
